9f663e3ac169e7be943a90c0d099199a8cb9c4fe
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-17 17:40:35 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :area_id
+    validates :theme_id
+  end
 end
