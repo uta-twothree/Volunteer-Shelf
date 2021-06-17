806fe0a3425b4c01d4c723118985a23213698f31
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-17 23:24:26 +0900

    ActiveHash導入に伴うバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,12 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :area_id
-    validates :theme_id
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :area_id
+      validates :theme_id
+    end
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
