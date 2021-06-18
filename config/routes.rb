0342b29c2e4d45a9c90d251f23a9d7413d70d21c
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-18 18:26:40 +0900

    ユーザー情報編集のための、ルーティング設定とアクション定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "albums#index"
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   resources :albums, only: [:index]
 end
