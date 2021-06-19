ddddfc06026eda77b441b31f0c3a716a793063c6
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-19 10:17:54 +0900

    中間テーブルの作成、マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_020930) do
+ActiveRecord::Schema.define(version: 2021_06_19_011440) do
+
+  create_table "album_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "album_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["album_id"], name: "index_album_users_on_album_id"
+    t.index ["user_id"], name: "index_album_users_on_user_id"
+  end
 
   create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,5 +49,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_020930) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "album_users", "albums"
+  add_foreign_key "album_users", "users"
   add_foreign_key "albums", "users"
 end
---
+++ spec/factories/album_users.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :album_user do
+    
+  end
+end
