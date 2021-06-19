48dab5e4a64837ea85ee83cb13bc3c378dd5770a
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-19 10:38:42 +0900

    albumモデルへのバリデーション定義

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,16 @@ class Album < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
   belongs_to :theme
-  
+
   has_many :users, through: :album_users
   has_many :album_users
+
+  with_options presence: true do
+    validates :name
+    validates :content
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :area_id
+      validates :theme_id
+    end
+  end
 end
