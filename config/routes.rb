b6e415156999b4e347333ddf5ec42bd8e76e5ec2
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-19 10:08:09 +0900

    アルバム作成機能のための、ルーティング設定とアクション定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "albums#index"
   resources :users, only: [:show, :edit, :update]
-  resources :albums, only: [:index]
+  resources :albums, only: [:index, :new, :create]
 end
