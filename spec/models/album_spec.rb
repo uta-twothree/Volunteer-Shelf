d6be92500b6c13bbbed1d71bbfa3d3b94e27005e
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-19 12:32:14 +0900

    アルバム作成機能のテストコードの実装

---
+++ spec/models/album_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe Album, type: :model do
+  before do
+    @album = FactoryBot.build(:album)
+  end
+
+  describe '作成するアルバムの登録' do
+    context 'アルバムを登録できるとき' do
+      it 'nameとcontent、areaとthemeが存在すれば登録できる' do
+        expect(@album).to be_valid
+      end
+    end
+
+    context 'アルバムを登録できないとき' do
+      it 'nameが空では登録できない' do
+        @album.name = ''
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'contentが空では登録できない' do
+        @album.content = ''
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it 'areaが空では登録できない' do
+        @album.area_id = ''
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Area can't be blank")
+      end
+
+      it 'areaのidが1の時は登録できない' do
+        @album.area_id = 1
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Area can't be blank")
+      end
+
+      it 'themeが空では登録できない' do
+        @album.theme_id = ''
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Theme can't be blank")
+      end
+
+      it 'themeのidが1の時は登録できない' do
+        @album.theme_id = 1
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Theme can't be blank")
+      end
+    end
+  end
+end
