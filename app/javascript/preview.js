009b795bd2643e6e16c2ffc3e9d4acc12cc86b5e
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-20 15:11:27 +0900

    アルバム紹介画像のプレビュー表示機能実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if ( document.getElementById('album-image')){
+    const ImageList = document.getElementById('image-list');
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', 'image-content')
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('album-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  }
+});
