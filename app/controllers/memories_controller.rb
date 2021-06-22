ed63fe12c8dba7e9bf83ebae201a77d474176d7b
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-22 18:25:29 +0900

    newアクション、createアクション内の編集

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,7 +1,21 @@
 class MemoriesController < ApplicationController
   def new
+    @album = Album.find(params[:album_id])
+    @memory = Memory.new
   end
 
   def create
+    @memory = Memory.new(memory_params)
+    if @memory.save
+      redirect_to "albums/show"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def memory_params
+    params.require(:memory).permit(:title, :text, :date, images: []).merge(user_id: current_user.id, album_id: params[:album_id])
   end
 end
