79f88eace8666e455b01ece41c5efd42fdf7d649
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-22 11:52:56 +0900

    Memoryモデルのバリデーション定義

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,4 +2,11 @@ class Memory < ApplicationRecord
   belongs_to :user
   belongs_to :album
   has_many_attached :images
+
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :date
+    validates :images
+  end
 end
