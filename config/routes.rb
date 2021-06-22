c442593e692441f7f2a5ff383d63cb1826900ed7
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-22 12:06:14 +0900

    メモリー作成のルーティング設定とアクション定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "albums#index"
   resources :users, only: [:show, :edit, :update]
-  resources :albums, only: [:index, :new, :create, :show, :edit, :update]
+  resources :albums, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :memories, only: [:new, :create]
+  end
 end
