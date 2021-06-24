a94858ec1d5ae65e6841a0b679957d80816a6dad
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-24 12:28:52 +0900

    updateアクション内の編集とコントローラー内でのアクセル制限

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,6 +1,8 @@
 class MemoriesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show]
-  before_action :set_album, only: [:new, :create, :show]
+  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update]
+  before_action :set_album, only: [:new, :create, :show, :edit, :update]
+  before_action :find_memory, only: [:show, :edit, :update]
+  before_action :move_to_album_show, only: [:show, :edit, :update]
   def new
     @memory = Memory.new
   end
@@ -15,19 +17,33 @@ class MemoriesController < ApplicationController
   end
 
   def show
-    @memory = @album.memories.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @memory.update(memory_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
 
   def set_album
     @album = Album.find(params[:album_id])
+  end
+
+  def find_memory
+    @memory = @album.memories.find(params[:id])
+  end
+
+  def move_to_album_show
+    unless current_user.id == @memory.user_id
+      redirect_to album_path(@album.id)
+    end
   end
 
   def memory_params
