da60190ea094b25449e9311d377e4efed448a3dd
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-28 11:56:59 +0900

    Commentモデルとマイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_020034) do
+ActiveRecord::Schema.define(version: 2021_06_28_025035) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2021_06_22_020034) do
     t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "text", null: false
+    t.bigint "user_id"
+    t.bigint "album_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["album_id"], name: "index_comments_on_album_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "memories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "text", null: false
@@ -74,6 +84,8 @@ ActiveRecord::Schema.define(version: 2021_06_22_020034) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "albums", "users"
+  add_foreign_key "comments", "albums"
+  add_foreign_key "comments", "users"
   add_foreign_key "memories", "albums"
   add_foreign_key "memories", "users"
 end
