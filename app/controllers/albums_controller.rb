8dcea53be792c03a7062f796d4b37f1f480d2780
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-28 14:41:25 +0900

    アルバム詳細画面にコメント投稿欄を実装

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -22,7 +22,7 @@ class AlbumsController < ApplicationController
   def show
     @memories = @album.memories.all.order('created_at DESC')
     @comment = Comment.new
-    @comments = @album.comments.includes(:user)
+    @comments = @album.comments.includes(:user).order('created_at DESC')
   end
 
   def edit
