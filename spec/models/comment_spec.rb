f8c40e38f896f4206bcc925717c998ccc1fc295e
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-28 19:27:09 +0900

    コメント保存のためのテストコード実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe '投稿するコメントの登録' do
+    context 'コメントを保存できるとき' do
+      it 'textが存在していれば保存できること' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントを保存できないとき' do
+      it 'textが空では保存できないこと' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+
+      it 'albumが紐付いていないと保存できないこと' do
+        @comment.album = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Album must exist")
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
